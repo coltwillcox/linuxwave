8089e552d0470608d4d6479139d35fa66c2aa2c1
Colt Willcox <necromanga@gmail.com>
2025-04-14 11:12:02 +0200

    Update waybar config

--- .config/waybar/scripts/netspeed.sh
+++ .config/waybar/scripts/netspeed.sh
@@ -7,20 +7,24 @@ max_upload=31457280    # ~30 Mbps
 download_icon="\udb83\uddb9"
 upload_icon="\udb83\uddbc"
 
-output=$(dool -n --noheaders --noupdate --bits --nocolor --ascii --integer 1 2 | tail -2 | tr k K | tr -d b | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
-array=($output)
+while :; do
 
-download=$(numfmt --from=iec --to-unit=1 $array[3])
-upload=$(numfmt --from=iec --to-unit=1 $array[4])
+	output=$(dool -n -N eno1 --noheaders --noupdate --bits --nocolor --ascii --integer 1 2 | tail -2 | tr k K | tr -d b | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
+	array=($output)
 
-download_percentage=$((100 * $download / $max_download))
-if [ $download_percentage -lt 0 ]; then download_percentage=0; fi
-if [ $download_percentage -gt 100 ]; then download_percentage=100; fi
-upload_percentage=$((100 * $upload / $max_upload))
-if [ $upload_percentage -lt 0 ]; then upload_percentage=0; fi
-if [ $upload_percentage -gt 100 ]; then upload_percentage=100; fi
+	download=$(numfmt --from=iec --to-unit=1 $array[1])
+	upload=$(numfmt --from=iec --to-unit=1 $array[2])
 
-text="$download_icon$download_percentage%$upload_percentage$upload_icon"
-tooltip="$download_icon$array[3] $upload_icon$array[4]"
+	download_percentage=$((100 * $download / $max_download))
+	if [ $download_percentage -lt 0 ]; then download_percentage=0; fi
+	if [ $download_percentage -gt 100 ]; then download_percentage=100; fi
+	upload_percentage=$((100 * $upload / $max_upload))
+	if [ $upload_percentage -lt 0 ]; then upload_percentage=0; fi
+	if [ $upload_percentage -gt 100 ]; then upload_percentage=100; fi
 
-printf '{"text": "%s", "tooltip": "%s"}' "$text" "$tooltip"
+	text="$download_icon$download_percentage%$upload_percentage$upload_icon"
+	tooltip="$download_icon$array[1] $upload_icon$array[2]"
+
+	printf '{"text": "%s", "tooltip": "%s"}\n' "$text" "$tooltip"
+
+done
